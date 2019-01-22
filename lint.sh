9ed54474efc2c2b55ecc415fbb49e4fba7bac481
wi1dcard <wi1dcard.cn@gmail.com>
2019-01-22 23:46:11 +0800

    SImple lint script for checking category naming style.

---
+++ lint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $(dirname $0)/source/_posts
+
+file_list=$(grep -LE '^categories:(\s[A-Z][a-zA-Z]*)+$' *)
+
+if [ -n "$file_list" ]
+then
+  echo "Please review the category name of following files:"
+  echo
+  echo $file_list
+
+  exit 1
+fi
+
+exit 0
