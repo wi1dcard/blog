4ba8181bb45a5affdd1b07d3bce6301ecadae3fd
wi1dcard <wi1dcard.cn@gmail.com>
2019-01-24 01:12:18 +0800

    Add a script helps me export to png from screenshots.

---
+++ image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dir=$(dirname $0)
+tmp_file=$(mktemp)
+
+pngpaste $tmp_file \
+&& hash=$(md5 < $tmp_file) \
+&& img="/resources/$hash.png" \
+&& mv $tmp_file "$dir/source$img" \
+&& echo "![]($img)" | tee >(pbcopy)
