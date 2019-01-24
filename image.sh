ef1df80d34080a23929f8e351da55a92a803a4b0
Weizhe Sun <weizhe.sun@rightcapital.com>
2019-01-24 10:20:55 +0000

    Use `bash -euo pipefail`

--- image.sh
+++ image.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/bash -eu
 
 dir=$(dirname $0)
 tmp_file=$(mktemp)
 
-pngpaste $tmp_file \
-&& hash=$(md5 < $tmp_file) \
-&& img="/resources/$hash.png" \
-&& mv $tmp_file "$dir/source$img" \
-&& echo "![]($img)" | tee >(pbcopy)
+pngpaste $tmp_file
+
+hash=$(md5 < $tmp_file)
+img="/resources/$hash.png"
+
+mv $tmp_file "$dir/source$img"
+echo "![]($img)" | tee >(pbcopy)
