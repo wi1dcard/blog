e985148d5d2ce6788b092177186516c5f756c650
wi1dcard <wi1dcard.cn@gmail.com>
2019-08-24 15:53:49 +0800

    Improve CI and the build script.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,7 +6,7 @@ echo "Loging in Docker Hub..."
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 echo "Pushing images to Docker Hub..."
-docker push $DOCKER_IMAGE
+docker push "$DOCKER_IMAGE"
 
 echo "Applying helm releases..."
 docker run --rm -v $(pwd)/deploy:/deploy \
