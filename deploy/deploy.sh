f93093bd80ec0e241ceb84acdf772ec0913744bf
wi1dcard <wi1dcard.cn@gmail.com>
2019-08-24 11:12:43 +0800

    Deploy to GKE!

---
+++ deploy/deploy.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+set -euo pipefail
+
+echo "Loging in Docker Hub..."
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+echo "Pushing images to Docker Hub..."
+docker push $DOCKER_IMAGE
+
+echo "Applying helm releases..."
+docker run --rm -v $(pwd)/deploy:/deploy \
+    -e HELM_TILLER_STORAGE=configmap \
+    -e KUBECONFIG_BASE64="$KUBECONFIG_BASE64" \
+    -e DOCKER_TAG="$DOCKER_TAG" \
+    -e INGRESS_HOST="$INGRESS_HOST" \
+    wi1dcard/helmfile-tillerless /deploy/helmfile.sh
---
+++ deploy/helmfile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Decoding kubeconfig..."
+mkdir ~/.kube
+UMASK=$(umask)
+umask 177
+base64 -d <<< "$KUBECONFIG_BASE64" > ~/.kube/config
+umask $UMASK
+
+set -x
+
+helm init --client-only
+helmfile -f deploy/helmfile.yaml apply
